2dc1c0e98c48ca42ade501b315289e6652b49dda
juan <juan.villaverde.rodriguez@udc.es>
2021-11-09 19:32:40 +0100

    Version 5, e1 casi acabado. e2, e3, e4 sin empezar

--- DS-32-08-B2/src/e1/Colegio.java
+++ DS-32-08-B2/src/e1/Colegio.java
@@ -35,27 +35,6 @@ public class Colegio {
 
         return listadoSalarios.toString();
     }
-    public static void main(String[] args) {
-        Colegio c1 = new Colegio();
-        Integrantes i1 = new Conserjes("pepe", "manuel", 18, 1, 2);
-        Integrantes i2 = new Conserjes("paco", "eeee", 23, 1, 2);
-        Integrantes i3 = new Guardabosques("pepa", "pig", 46, 5,4);
-        Integrantes i4 = new Fantasmas("maria", "ee", 46, Residentes.Casas.Gryffindor,2);
-        Integrantes i5 = new Estudiantes("laura", "ciii", 46, Residentes.Casas.Gryffindor,0);
-        Integrantes i6 = new Docentes("pepito", "grillo", 2,Docentes.Asignaturas.Herbologia ,3);
-
-
-        c1.x.add(i1);
-        c1.x.add(i2);
-        c1.x.add(i3);
-        c1.x.add(i4);
-        c1.x.add(i5);
-        c1.x.add(i6);
-
-        //System.out.println(c1.imprimirRecompensas());
-        System.out.println(c1.imprimirSalarios());
-
-    }
 
 }
 
